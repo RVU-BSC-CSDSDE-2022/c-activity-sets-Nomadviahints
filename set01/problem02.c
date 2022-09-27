b4882ab3b926b1b4083d3a24427671091f646463
Nomadviahints <114479036+Nomadviahints@users.noreply.github.com>
2022-09-27 15:09:54 +0530

    Update problem02.c

---
+++ set01/problem02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ main()
+{
+  int w = 60;
+  int x = 70;
+  int y = 30;
+  int a = (w + x + y);
+  printf("Sum of %d, %d, %d is %d\n", w, x, y, a);
+  int z = (w + x + y)/3;
+   printf("Average of %d, %d,%d is %d", w, x, y,z); 
+}
